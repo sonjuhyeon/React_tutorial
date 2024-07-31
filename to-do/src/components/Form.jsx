b91e2ac2142ba70a01af81e9bd0422ce29bf2e2f
sonjuhyeon <ths120521113105@daum.net>
2024-07-31 12:52:32 +0900

    modified todo

---
+++ to-do/src/components/Form.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+const Form = ({ createTodo }) => {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form className="font-primary flex" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="할 일을 입력해주세요"
+        className="outline-none bg-transparent border border-gray-500 p-4 w-[80%] text-gray-600 rounded placeholder:text-gray-400"
+        onChange={(e) => setValue(e.target.value)}
+        value={value}
+      />
+      <button
+        className="bg-gray-700 border-none p-2 text-gray-300
+      rounded ml-2 w-[20%]"
+      >
+        할일 추가
+      </button>
+    </form>
+  );
+};
+
+export default Form;
