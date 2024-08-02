af8213d7891125848c93da65773c7de1f4562ab7
sonjuhyeon <ths120521113105@daum.net>
2024-08-02 12:09:22 +0900

    add navigation in template

--- react_template/src/components/ModeCtrl.jsx
+++ react_template/src/components/ModeCtrl.jsx
@@ -1,13 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { MdOutlineWbSunny } from "react-icons/md";
 import { IoMoon } from "react-icons/io5";
+import "./Navbar.css";
 
 const ModeCtrl = () => {
   const [darkMode, setDarkMode] = useState(true);
 
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+      document.documentElement.style.setProperty("color-scheme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      document.documentElement.style.setProperty("color-scheme", "light");
+    }
+  }, [darkMode]);
+
   return (
-    <button onClick={() => setDarkMode(!darkMode)}>
-      {darkMode ? <IoMoon /> : <MdOutlineWbSunny />}
+    <button
+      className="mode_btn dark:bg-neutral-300 dark:text-neutral-950 bg-neutral-950 text-neutral-300 lg:p-2 ml-1 lg:text-2xl p-1 md:text-xl"
+      onClick={() => setDarkMode(!darkMode)}
+    >
+      {darkMode ? <MdOutlineWbSunny /> : <IoMoon />}
     </button>
   );
 };
--- react_template/src/components/Navbar.jsx
+++ react_template/src/components/Navbar.jsx
@@ -1,35 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 import { authLink, navItems } from "../constants/data";
 import ModeCtrl from "./ModeCtrl";
+import { Menu, X } from "lucide-react";
 
 const Navbar = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   return (
     <nav>
-      <div className="container">
+      <div className="container relative">
         <div className="nav_wrapper">
           <div className="logo">
             <div className="logo_symbol">
               <span className="logo_dot"></span>
             </div>
-            <span className="logo_title">AceDot.Dev</span>
+            <span className="logo_title lg:text-xl md:text-lg">AceDot.Dev</span>
           </div>
-          <ul className="navi">
+          <ul className="navi hidden lg:flex">
             {navItems.map((item, idx) => (
               <li key={idx}>
                 <Link to={item.to}>{item.label}</Link>
               </li>
             ))}
           </ul>
-          <div className="sub_links">
+          <div className="sub_links hidden lg:flex">
             {authLink.map((item, idx) => (
               <Link
                 key={idx}
                 to={item.to}
                 className={`${
                   idx === 1 &&
-                  "bg-gradient-to-r from-indigo-500 to bg-indigo-700"
+                  "bg-gradient-to-r from-indigo-500 to bg-indigo-700 text-white"
                 }`}
               >
                 {item.label}
@@ -37,7 +39,43 @@ const Navbar = () => {
             ))}
             <ModeCtrl />
           </div>
+          <div className="menu_icon lg:hidden flex gap-x-2">
+            <ModeCtrl />
+            <button onClick={() => setMobileMenuOpen(!mobileMenuOpen)}>
+              {mobileMenuOpen ? <X /> : <Menu />}
+            </button>
+          </div>
         </div>
+        {mobileMenuOpen && (
+          <div className="sub_nav_mobile absolute bg-white w-full top-full left-0 dark:bg-neutral-900 mt-[1px] lg:hidden">
+            <ul className="navi">
+              {navItems.map((item, idx) => (
+                <li
+                  key={idx}
+                  className="py-4 border-b border-neutral-600 w-full text-center"
+                >
+                  <Link to={item.to} className="block w-full h-full">
+                    {item.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            <div className="sub_links flex py-4 gap-2 justify-center border-b border-neutral-600">
+              {authLink.map((item, idx) => (
+                <Link
+                  key={idx}
+                  to={item.to}
+                  className={`${
+                    idx === 1 &&
+                    "bg-gradient-to-r from-indigo-500 to bg-indigo-700 text-white"
+                  }`}
+                >
+                  {item.label}
+                </Link>
+              ))}
+            </div>
+          </div>
+        )}
       </div>
     </nav>
   );
